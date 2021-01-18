6809f37b657021c79deeca9a70e6306ef29a5bb6
PreacherDHM <jesseh2003@outlook.com>
2021-01-17 16:56:33 -0800

    Added a pid to the track command

--- 2021_VisionTeam/src/main/java/frc/robot/RobotContainer.java
+++ 2021_VisionTeam/src/main/java/frc/robot/RobotContainer.java
@@ -7,10 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.Util.XboxGamepad;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.drivers.LimeLight;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.SS_DriveTrain;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /**
@@ -26,9 +23,7 @@ public class RobotContainer {
  private static final LimeLight limeLight = new LimeLight();
 
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
--- 2021_VisionTeam/src/main/java/frc/robot/commands/C_Track.java
+++ 2021_VisionTeam/src/main/java/frc/robot/commands/C_Track.java
@@ -3,42 +3,65 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-
+import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
+import frc.robot.Util.XboxGamepad;
 import frc.robot.drivers.LimeLight;
 import frc.robot.subsystems.SS_DriveTrain;
 
 public class C_Track extends CommandBase {
   /** Creates a new C_Track. */
 
-  private final int SPEED_OFFSET = 1;
+  //Constants
+  //PID Tolerance
+  private final double ROTATION_TOLERANCE = 1;
+  private final double SETPOINT_POSITION = 0;
 
+  private final double kP = .0005;
+  private final double kI = 0.0;
+  private final double kD = 0.0;
+
+  //Controllers
+  private XboxGamepad driveController = RobotContainer.getXboxController();
+
+  //Robot Componets
   private LimeLight limeLight = RobotContainer.getLimeLight();
   private SS_DriveTrain driveTrain = SS_DriveTrain.getinstance();
 
+  private PIDController pid = new PIDController(kP,kI,kD);
+
+
   public C_Track() {
+
+    //init PID
+    pid.setSetpoint(SETPOINT_POSITION);
+    pid.setTolerance(ROTATION_TOLERANCE);
+
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(driveTrain);
+    
+  }
 
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    
     //LimeLight Init
     //when you are initing the Lime Light you need to turn on the light
     //  and just in case i set the mode to default.
     limeLight.setCameraMode(LimeLight.CAMERA_DEFAULT_MODE);
     limeLight.setLEDMode(LimeLight.LED_ON);
+    
   }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     //turns tward the target when there is a target.
-    if(limeLight.getValidTarget()){
+    if(limeLight.getValidTarget() && !pid.atSetpoint()){
       //get a value between 0 and 1 with a speed offset.
-      double targetOffset = limeLight.getXOffset()/limeLight.getXOffset() + SPEED_OFFSET;
+      double targetOffset = pid.calculate(limeLight.getXOffset(),0);
       //replaces the power inputs as the targetOffset value.
       driveTrain.setPower(targetOffset, targetOffset * -1);
     }
@@ -54,7 +77,7 @@ public class C_Track extends CommandBase {
   }
 
   // Returns true when the command should end.
-  
+
   @Override
   public boolean isFinished() {
     return false;
