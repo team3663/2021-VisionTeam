4f9e5fdfa865677cc9f3ddbb91c5057a16daeafb
EricHoelscher <40500077+EricHoelscher@users.noreply.github.com>
2021-01-30 10:18:21 -0800

    Fix new pixy driver

--- 2021_VisionTeam/src/main/java/frc/robot/Robot.java
+++ 2021_VisionTeam/src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.C_Drive;
+import frc.robot.commands.PixyClassTest;
 import frc.robot.commands.PixyTest;
 
 /**
@@ -60,7 +61,7 @@ public class Robot extends TimedRobot {
 
   public void autonomousInit() {
 
-    new PixyTest().schedule();
+    new PixyClassTest().schedule();
 
     // m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
