b13cfc92a96cf2048a0df1116df927f436d79820
EricHoelscher <40500077+EricHoelscher@users.noreply.github.com>
2021-02-06 11:13:31 -0800

    add PatternRecognitionTest

---
+++ 2021_VisionTeam/src/main/java/frc/robot/commands/C_CheckPattern.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class C_CheckPattern extends CommandBase {
+
+  NetworkTableEntry pattern;
+
+  public C_CheckPattern() {
+    ShuffleboardTab tab = Shuffleboard.getTab("Pattern Training");
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
